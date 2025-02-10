99730c5de3686fc2e7cf8aa5295f6ac31240aed8
dukeduke00 <dusangostic69@gmail.com>
2025-02-10 18:29:26 +0100

    * Created migrations and models for users and products

---
+++ app/Models/Products.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Products extends Model
+{
+    protected $table = 'products';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'brand',
+        'category',
+        'image',
+        'price',
+        'quantity',
+    ];
+}
--- database/migrations/2025_02_10_171205_create_products_table.php
+++ database/migrations/2025_02_10_171205_create_products_table.php
@@ -11,15 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name', 64);
+            $table->text('description');
+            $table->string('brand', 64);
+            $table->string('category', 64);
+            $table->string('image', 255);
+            $table->decimal('price', 10, 2);
+            $table->integer('quantity');
             $table->timestamps();
-            $table->string('role', '20')->default('user');
         });
     }
 
@@ -28,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
